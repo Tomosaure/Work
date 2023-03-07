0418fd2254472b341fd33f99661a1da4b3b4355a
Tomosaure <bonetto.tom@gmail.com>
2023-03-07 13:53:06 +0100

    First commit

--- 2A/S8/TAV/donnees_TP1/moindres_carres.m
+++ 2A/S8/TAV/donnees_TP1/moindres_carres.m
@@ -2,5 +2,5 @@ function beta = moindres_carres(d,x,y)
     nparmik_mat = arrayfun(@(n) nchoosek(d,n), (1:d));
     A = nparmik_mat.*x.^(1:d).*(1-x).^(d-(1:d));
     B = y - y(1).*(1-x).^d;
-    beta = A\B
+    beta = A\B;
 end
