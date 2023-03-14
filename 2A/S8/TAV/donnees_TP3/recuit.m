3596106a5c16ffa74297f334eca4c12fd166e7bd
Tomosaure <bonetto.tom@gmail.com>
2023-03-14 14:41:05 +0100

    First commit

--- 2A/S8/TAV/donnees_TP3/recuit.m
+++ 2A/S8/TAV/donnees_TP3/recuit.m
@@ -4,6 +4,16 @@ function [U,k] = recuit(U,k,AD,T,beta)
         for j = 1:c
             valeur = setdiff(1:N,k);
             k_nouv = valeur(randi(numel(valeur)));
+            Us = AD(i,j,k_nouv) + beta*regularisation();
+            if Us < U
+                U(i,j) = Us;
+                k(i,j) = k_nouv;
+            else
+                if rand() < exp(-(Us-U(i,j))/T)
+                    U(i,j) = Us;
+                    k(i,j) = k_nouv;
+                end
+            end
 
         end
     end
